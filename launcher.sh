d10ac41876896abb84390145e719be06774b68af
Remus CURSARU <remus.cm+github@gmail.com>
2016-07-03 18:30:30 +0300

    Create launcher.sh

---
+++ launcher.sh
@@ -0,0 +1,3 @@
+#!/bin/sh                                                                                                                   
+echo 'launcher is online' > /home/pi/Pimoroni/logs/pianolog                                                                 
+python /home/pi/Pimoroni/pianohat/simple-piano.py >> /home/pi/Pimoroni/logs/pianolog &
